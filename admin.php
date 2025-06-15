80892714410eb0f1ade32e3647fa110ef1d11a88
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-15 15:37:59 -0300

    Ajustando o Bootstrap e aplicando seus atributos

--- admin.php
+++ admin.php
@@ -19,12 +19,12 @@ $result = $conn->query("SELECT * FROM agendamentos ORDER BY data_agendada, hora_
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin - Agendamentos</title>
-  <link rel="stylesheet" href="admin.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
-<body>
-  <h2>Lista de Agendamentos</h2>
-  <table>
-    <tr>
+<body class="bg-dark">
+  <h2 class="text-white text-center my-4">Lista de Agendamentos</h2>
+  <table class="table table-dark table-striped">
+    <tr class="table-light">
       <th>ID</th>
       <th>Nome</th>
       <th>Telefone</th>
